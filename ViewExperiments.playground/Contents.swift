6aeb0a4f775662326ec0e88546c325d2d9a76501
Averiser <averiser@gmail.com>
2022-07-22 15:30:32 +0300

    first commit - view in center of another view

---
+++ ViewExperiments.playground/Contents.swift
@@ -0,0 +1,74 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+     setupViews()
+    }
+  
+  // настройка представлений сцены
+  private func setupViews() {
+    self.view = getRootView()
+    let redView = getRedView()
+    let greenView = getGreenView()
+    
+    let whiteView = getWhiteView()
+    
+    set(view: greenView, toCenterOfView: redView)
+    
+    set(view: whiteView, toCenterOfView: redView)
+    
+    self.view.addSubview(redView)
+    redView.addSubview(greenView)
+    redView.addSubview(whiteView)
+  }
+  
+  // создание корневого представления
+  private func getRootView() -> UIView {
+    let view = UIView()
+    view.backgroundColor = .gray
+    return view
+  }
+  
+  // создание красного представления
+  private func getRedView() -> UIView {
+    let viewFrame = CGRect(x: 50, y: 50, width: 200, height: 200)
+    let view = UIView(frame: viewFrame)
+    view.backgroundColor = .red
+    view.clipsToBounds = true
+    return view
+  }
+  
+  private func getGreenView() -> UIView {
+    let viewFrame = CGRect(x: 10, y: 10, width: 180, height: 180)
+    let view = UIView(frame: viewFrame)
+    view.backgroundColor = .green
+    return view
+  }
+  
+  private func getWhiteView() -> UIView {
+    let viewFrame = CGRect(x: 0, y: 0, width: 50, height: 50)
+    let view = UIView(frame: viewFrame)
+    view.backgroundColor = .white
+    return view
+  }
+  
+  private func set(view moveView: UIView, toCenterOfView baseView: UIView) {
+    // размеры вложенного представления
+    let moveViewWidth = moveView.frame.width
+    let moveViewHeight = moveView.frame.height
+    
+    // размеры родительского представления
+    let baseViewWidth = baseView.frame.width
+    let baseViewHeight = baseView.frame.height
+    
+    // вычисление и изменение координат
+    let newXCoordinate = (baseViewWidth - moveViewWidth) / 2
+    let newYCoordinate = (baseViewHeight - moveViewHeight) / 2
+    moveView.frame.origin = CGPoint(x: newXCoordinate, y: newYCoordinate)
+  }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
