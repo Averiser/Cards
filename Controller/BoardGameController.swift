5ed95a0cc4dca4846405e5f6b3534a7035dbff0b
Averiser <averiser@gmail.com>
2022-07-28 00:55:40 +0300

    fourteenth - final strokes

--- Controller/BoardGameController.swift
+++ Controller/BoardGameController.swift
@@ -9,6 +9,107 @@ import UIKit
 
 class BoardGameController: UIViewController {
   
+  
+  private var flippedCards = [UIView]()
+  
+  // игральные карточки
+  var cardViews = [UIView]()
+  
+  private func placeCardsOnBoard(_ cards: [UIView]) {
+    // удаляем все имеющиеся на игровом поле карточки
+    for card in cardViews {
+      card.removeFromSuperview()
+    }
+    cardViews = cards
+    // перебираем карточки
+    for card in cardViews {
+      // для каждой карточки генерируем случайные координаты
+      let randomXCoordinate = Int.random(in: 0...cardMaxXCoordinate)
+      let randomYCoordinate = Int.random(in: 0...cardMaxYCoordinate)
+      card.frame.origin = CGPoint(x: randomXCoordinate, y: randomYCoordinate)
+      // размещаем карточку на игровом поле
+      boardGameView.addSubview(card)
+    }
+  }
+  
+  // генерация массива карточек на основе данных Модели
+  private func getCardsBy(modelData: [Card]) -> [UIView] {
+    // хранилище для представлений карточек
+    var cardViews = [UIView]()
+    // фабрика карточек
+    let cardViewFactory = CardViewFactory()
+    // перебираем массив карточек в Модели
+    for (index, modelCard) in modelData.enumerated() {
+      // добавляем первый экземпляр карты
+      let cardOne = cardViewFactory.get(modelCard.type, withSize: cardSize, andColor: modelCard.color)
+      cardOne.tag = index
+      cardViews.append(cardOne)
+      
+      // добавляем второй экземпляр карты
+      let cardTwo = cardViewFactory.get(modelCard.type, withSize: cardSize, andColor: modelCard.color)
+      cardTwo.tag = index
+      cardViews.append(cardTwo)
+    }
+    // добавляем всем картам обработчик переворота
+    for card in cardViews {
+      (card as! FlippableView).flipCompletionHandler = { [self] flippedCard in
+        // переносим карточку вверх иерархии
+        flippedCard.superview?.bringSubviewToFront(flippedCard)
+        
+        // добавляем или удаляем карточку
+        if flippedCard.isFlipped {
+          self.flippedCards.append(flippedCard)
+        } else {
+          if let cardIndex = self.flippedCards.firstIndex(of: flippedCard) {
+            self.flippedCards.remove(at: cardIndex)
+          }
+        }
+        
+        // если перевернуто 2 карточки
+        if flippedCards.count == 2 {
+          // получаем карточки из данных модели
+          let firstCard = game.cards[self.flippedCards.first!.tag]
+          let secondCard = game.cards[self.flippedCards.last!.tag]
+          
+          // если карточки одинаковые
+          if game.checkCards(firstCard, secondCard) {
+            // сперва анимировано скрываем их
+            UIView.animate(withDuration: 0.3) {
+              self.flippedCards.first!.layer.opacity = 0
+              flippedCards.last!.layer.opacity = 0
+              // после чего удаляем из иерархии
+            } completion: { _ in
+              self.flippedCards.first!.removeFromSuperview()
+              self.flippedCards.last!.removeFromSuperview()
+              self.flippedCards = []
+            }
+
+          } else {
+            // переворачиваем карточки рубашкой вверх
+            for card in self.flippedCards {
+              (card as! FlippableView).flip()
+            }
+          }
+        }
+      }
+    }
+    return cardViews
+  }
+  
+  // размеры карточек
+  private var cardSize: CGSize {
+    CGSize(width: 80, height: 120)
+  }
+  
+  // предельные координаты размещения карточки
+  private var cardMaxXCoordinate: Int {
+    Int(boardGameView.frame.width - cardSize.width)
+  }
+  
+  private var cardMaxYCoordinate: Int {
+    Int(boardGameView.frame.height - cardSize.height)
+  }
+  
   lazy var boardGameView = getBoardGameView()
   
   private func getBoardGameView() -> UIView {
@@ -94,7 +195,9 @@ class BoardGameController: UIViewController {
     }
   
   @objc func startGame(_ sender: UIButton) {
-    print("button was pressed")
+    game = getNewGame()
+    let cards = getCardsBy(modelData: game.cards)
+    placeCardsOnBoard(cards)
   }
     
 
